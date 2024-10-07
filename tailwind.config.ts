f602a3079f9eb2c9d76ba2211750a1b0501c8ebc
MiladSedhom <MiladSedhom@gmail.com>
2024-10-07 22:09:08 +0300

    feat: setup colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,18 @@
 export default {
 	theme: {
 		extend: {
-			colors: {},
+			colors: {
+				primary: '#D20653',
+				secondary: '#FF951D',
+				'yellow-1': '#F8D486',
+				fg: '#333333',
+				'gray-1': '#4F4F4F',
+				'gray-2': '#828282',
+				'gray-3': '#E0E0E0',
+			},
+		},
+		backgroundImage: {
+			'primary-gradient': 'linear-gradient(90deg, #D20653 0%, #FF951D 100%)',
 		},
 	},
 }
