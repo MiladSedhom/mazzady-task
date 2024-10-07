649c4407574df7e4c5e3ae65135d33d0c5375770
MiladSedhom <MiladSedhom@gmail.com>
2024-10-07 17:49:43 +0300

    feat: setup tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<NuxtRouteAnnouncer />
+	<div class="bg-zinc-800 text-white w-screen h-screen">
+		<div class="p-8">
+			<h1 class="text-2xl">Hiiiii🍫🍫</h1>
+		</div>
+	</div>
 </template>
