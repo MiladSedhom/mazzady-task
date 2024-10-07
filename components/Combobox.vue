c6018c36f8b35c4bcbab7d8898186ff35ca3cba2
MiladSedhom <MiladSedhom@gmail.com>
2024-10-08 01:41:16 +0300

    fix: shows label even if not binded

--- components/Combobox.vue
+++ components/Combobox.vue
@@ -4,25 +4,37 @@ interface Option {
 	value: string | number
 }
 
-defineProps({
+const props = defineProps({
 	modelValue: { type: [String, Number], default: '' },
+	emptyMsg: { type: [String, Number], default: 'No options' },
+	placeholder: { type: String, default: 'Placeholder...' },
 	options: { type: Array<Option>, default: [] },
 })
+
+const emit = defineEmits<{
+	'update:modelValue': [value: string | number]
+}>()
+
+const v = ref('')
+const selectedLabel = computed(() => props.options.find(o => o.value === v.value)?.label!)
+
+watchEffect(
+	() => {
+		emit('update:modelValue', v.value)
+	},
+	{ flush: 'pre' }
+)
 </script>
 
 <template>
-	<ComboboxRoot
-		:model-value="modelValue"
-		@update:model-value="v => $emit('update:modelValue', v)"
-		class="relative max-w-[20rem]"
-	>
+	<ComboboxRoot v-model="v" class="relative max-w-[20rem]" :displayValue="() => selectedLabel">
 		<ComboboxAnchor
 			class="min-w-[20rem] inline-flex items-center justify-between rounded px-4 text-sm leading-none h-10 gap-[5px] bg-bg text-fg shadow-[0_2px_10px] shadow-black/10 hover:bg-yellow-1/30 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-gray-2/50 outline-none"
 		>
 			<ComboboxInput
 				class="!bg-transparent outline-none text-fg h-full placeholder-gray-2/50 grow"
-				placeholder="Placeholder..."
-				:value="options.find(o => o.value === modelValue)?.label"
+				:placeholder="placeholder"
+				v-bind="$attrs"
 			/>
 			<ComboboxTrigger>
 				<Icon name="radix-icons:chevron-down" class="h-5 w-5 text-fg" />
@@ -32,7 +44,7 @@ defineProps({
 		<ComboboxContent
 			class="absolute z-10 w-full mt-2 min-w-[20rem] bg-bg overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
 		>
-			<ComboboxEmpty class="text-fg text-xs font-medium text-center py-2" />
+			<ComboboxEmpty class="text-fg text-xs font-medium text-center py-2"> {{ emptyMsg }}</ComboboxEmpty>
 
 			<ComboboxGroup>
 				<ComboboxItem
