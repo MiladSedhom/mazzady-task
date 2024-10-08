b17e52d83a49876091160350d5e5c40a6cd93735
MiladSedhom <MiladSedhom@gmail.com>
2024-10-08 22:56:32 +0300

    feat: product card

---
+++ components/product/Card.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="rounded-[34xp] flex items-center w-full">
+		<img class="rounded-[28px] m-4" src="/public/product.png" alt="product" width="145" height="127" />
+		<div class="grow">
+			<div class="flex justify-between">
+				<p class="text-sm font-medium">Six-piece clothing set (blouse - pants - hat and ...</p>
+				<Icon name="radix-icons:heart" class="w-6 h-6" />
+			</div>
+			<p class="text-lg font-normal text-gray-1">
+				Starting Price <span class="font-bold text-xl text-fg">1000 EGP</span>
+			</p>
+			<div>
+				<span class="text-lg font-normal text-gray-1">Lot starts in </span>
+
+				<div class="inline-flex gap-4">
+					<div class="leading-10 bg-yellow-1 text-secondary rounded-3xl text-sm px-3">
+						<span class="text-xl font-bold"> 2 </span> Days
+					</div>
+					<div class="leading-10 bg-yellow-1 text-secondary rounded-3xl text-sm px-3">
+						<span class="text-xl font-bold"> 10 </span> Hours
+					</div>
+					<div class="leading-10 bg-yellow-1 text-secondary rounded-3xl text-sm px-3">
+						<span class="text-xl font-bold"> 50 </span> Minutes
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,37 @@
 <template>
-	<Header />
-	<div class="px-[5.25rem] py-[3rem] flex">
-		<div>
-			<ProfileCard />
-			<div class="mb-6"></div>
-			<QRCodeCard />
+	<div class="w-full h-full">
+		<Header />
+		<div class="px-[5.25rem] py-[3rem] flex gap-6">
+			<div>
+				<ProfileCard />
+				<div class="mb-6"></div>
+				<QRCodeCard />
+			</div>
+			<div class="bg-bg p-6 rounded-[21px] grow max-h-[125vh] overflow-y-auto">
+				<div class="flex justify-between">
+					<div class="flex gap-2.5">
+						<button
+							class="w-[6.75rem] border border-seconday text-secondary bg-yellow-1 py-2 rounded-[14px]"
+						>
+							Products
+						</button>
+						<button class="w-[6.75rem] border border-gray-3 py-2 rounded-[14px]">Articles</button>
+						<button class="w-[6.75rem] border border-gray-3 py-2 rounded-[14px]">Reviews</button>
+					</div>
+					<button
+						class="bg-primary-gradient leading-10 text-bg text-sm font-bold px-4 rounded-[10px] hidden sm:block"
+					>
+						<Icon name="radix-icons:plus-circled" />
+						Add Review
+					</button>
+				</div>
+				<h1 class="font-extrabold text-[2rem] my-6">
+					Products <span class="text-gray-2 text-sm font-medium">(12)</span>
+					<div>
+						<ProductCard v-for="i in Array(18)" />
+					</div>
+				</h1>
+			</div>
 		</div>
 	</div>
 </template>
