104ba349227aaba8deb2f89703f1bf9c8e3594ed
MiladSedhom <MiladSedhom@gmail.com>
2024-10-08 15:01:58 +0300

    feat: others

--- components/CatsForm.vue
+++ components/CatsForm.vue
@@ -7,23 +7,23 @@ const selectedCatId = ref()
 const subCats = computed(() => cats?.find(c => c.id === selectedCatId.value)?.children)
 const selectedSubCatId = ref()
 
-const properties = ref<Property[]>([])
+//{propertyId: optionsId | 'other'}
+const selectedProperties = ref<Record<number, number | string>>({})
+
+// {propertyId: value}
+const otherOptions = ref<Record<number, string>>({})
+
+let properties = ref<Awaited<ReturnType<typeof useProperties>>>()
+
 watchEffect(async () => {
 	if (selectedSubCatId.value) {
-		const { data } = await useProperties(selectedSubCatId.value)
-		properties.value = data ?? []
+		properties.value = await useProperties(selectedSubCatId.value)
 	}
-})
-
-const selectedPropertiesIds = ref<number[]>([])
-
-watchEffect(() => {
-	console.log(selectedPropertiesIds.value)
 })
 </script>
 
 <template>
-	<form action="" class="bg-bg rounded-[21px] w-96 h-96 p-8 flex flex-col gap-4 overflow-auto">
+	<form action="" class="bg-bg rounded-[21px] w-[30rem] h-96 p-8 flex flex-col gap-4 overflow-auto items-center">
 		<div>
 			<label for="main-cat" class="text-sm font-medium"> Main Category</label>
 			<Combobox
@@ -44,12 +44,14 @@ watchEffect(() => {
 			/>
 		</div>
 
-		<div v-for="p in properties">
+		<div v-for="p in properties?.data">
 			<label :for="p.slug" class="text-sm font-medium"> {{ p.name }}</label>
 			<Combobox
-				@update:modelValue="v => {selectedPropertiesIds.push(v as number)
-					console.log(selectedPropertiesIds)
-				}"
+				@update:modelValue="
+					v => {
+						if (v !== '') selectedProperties[p.id] = v
+					}
+				"
 				:options="[
 					...p.options?.map(o => ({ label: o.name, value: o.id })),
 					{ label: 'other', value: 'other' },
@@ -57,6 +59,13 @@ watchEffect(() => {
 				placeholder="Subcategory..."
 				emptyMsg="No subcategories, try choosing a main categorey."
 				:name="p.slug"
+			/>
+			<input
+				v-bind:value="otherOptions[p.id]"
+				v-if="selectedProperties[p.id] === 'other'"
+				class="h-10 w-full text-2 px-4 text-fg placeholder-gray-2/50 grow shadow-[0_2px_10px] shadow-black/10 hover:bg-yellow-1/30 data-[placeholder]:text-gray-2/50 outline-none"
+				type="text"
+				placeholder="Enter a value..."
 			/>
 		</div>
 	</form>
