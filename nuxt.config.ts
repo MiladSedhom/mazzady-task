b86b6a5b2182c4b2ca6b60bd2500fb991e35932e
MiladSedhom <MiladSedhom@gmail.com>
2024-10-08 18:36:56 +0300

    feat: setup font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', 'radix-vue/nuxt', '@nuxt/icon'],
-	runtimeConfig: {
-		public: {
-			apiBase: 'https://staging.mazaady.com/api/v1/',
-			privateKey: process.env.PRIVATE_KEY,
-		},
-	},
+				compatibilityDate: '2024-04-03',
+				devtools: { enabled: true },
+				modules: [
+				 '@nuxtjs/tailwindcss',
+				 'radix-vue/nuxt',
+				 '@nuxt/icon',
+				 '@nuxt/fonts',
+				],
+				runtimeConfig: {
+								public: {
+												apiBase: 'https://staging.mazaady.com/api/v1/',
+												privateKey: process.env.PRIVATE_KEY,
+								},
+				},
 })
