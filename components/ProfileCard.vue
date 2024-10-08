d58db18be48091ab2a3741fcac8e07e3176c3e2d
MiladSedhom <MiladSedhom@gmail.com>
2024-10-08 23:54:09 +0300

    feat: responsive

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,30 +1,30 @@
 <template>
 	<div class="bg-bg p-6 rounded-[21px] w-full">
-		<img src="/public//placeholder.png" alt="avatar" class="mb-4" />
-		<h1 class="text-2xl font-bold">Hala Ahmed</h1>
-		<p class="mb-6">
+		<img src="/public//placeholder.png" alt="avatar" class="w-20 h-20 sm:w-auto sm:h-auto mb-4" />
+		<h1 class="text-lg sm:text-2xl font-bold">Hala Ahmed</h1>
+		<p class="text-sm sm:text-base mb-6">
 			I am Hala Ahmed, I am the owner of the local brand called Beauty which is for Mackeup and Skin Care.
 		</p>
 		<div class="flex justify-center items-center gap-4">
-			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-2 items-end">
+			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-1 sm:gap-2 items-end">
 				<Icon name="radix-icons:person" class="w-6 h-6 mb-1.5" />
 				<div>
-					<p class="font-bold text-fg -my-1.5">5</p>
-					<span class="text-xs">Following</span>
+					<p class="font-bold text-sm sm:text-base text-fg -my-1.5">5</p>
+					<span class="text-[10px] sm:text-xs">Following</span>
 				</div>
 			</div>
-			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-2 items-end">
+			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-1 sm:gap-2 items-end">
 				<Icon name="radix-icons:person" class="w-6 h-6 mb-1.5" />
 				<div>
-					<p class="font-bold text-fg -my-1.5">5</p>
-					<span class="text-xs">Following</span>
+					<p class="font-bold text-sm sm:text-base text-fg -my-1.5">5</p>
+					<span class="text-[10px] sm:text-xs">Following</span>
 				</div>
 			</div>
-			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-2 items-end">
+			<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-1 sm:gap-2 items-end">
 				<Icon name="radix-icons:person" class="w-6 h-6 mb-1.5" />
 				<div>
-					<p class="font-bold text-fg -my-1.5">5</p>
-					<span class="text-xs">Following</span>
+					<p class="font-bold text-sm sm:text-base text-fg -my-1.5">5</p>
+					<span class="text-[10px] sm:text-xs">Following</span>
 				</div>
 			</div>
 		</div>
--- components/QRCodeCard.vue
+++ components/QRCodeCard.vue
@@ -1,19 +1,29 @@
+<script setup lang="ts">
+const expanded = ref(false)
+</script>
+
 <template>
 	<div class="bg-bg p-6 rounded-[21px] max-w-[25rem]">
-		<div class="flex justify-between">
+		<div class="flex justify-between items-center">
 			<h1 class="text-2xl font-bold">QR Code</h1>
-			<div class="flex gap-6">
-				<Icon name="radix-icons:eye-open" class="w-6 h-6 inline-block" />
-				<Icon name="radix-icons:share-1" class="w-6 h-6 inline-block" />
-				<Icon name="radix-icons:download" class="w-6 h-6 inline-block" />
+			<div class="flex gap-6 items-center">
+				<Icon name="radix-icons:eye-open" class="w-5 h-5 sm:w-6 sm:h-6 inline-block" />
+				<Icon name="radix-icons:share-1" class="w-5 h-5 sm:w-6 sm:h-6 inline-block" />
+				<Icon name="radix-icons:download" class="w-5 h-5 sm:w-6 sm:h-6 inline-block" />
+				<button @click="expanded = !expanded" class="bg-gray-3 rounded-full w-5 h-5 sm:w-6 sm:h-6 sm:hidden">
+					<Icon name="radix-icons:caret-down" class="w-5 h-5 sm:w-6 sm:h-6 text-fg inline-block" />
+				</button>
 			</div>
 		</div>
-		<div class="bg-yellow-1 text-secondary rounded-[18px] p-3 flex gap-1 items-center my-4">
+		<div
+			class="bg-yellow-1 text-secondary rounded-[18px] p-3 gap-1 items-center my-4 hidden sm:flex"
+			:class="expanded && '!flex'"
+		>
 			<Icon name="radix-icons:file" class="w-6 h-6 inline-block" />
 			<p class="text-fg text-xs inline-block">Download the QR code or share it with your friends.</p>
 		</div>
 
-		<div class="p-5 bg-primary-gradient rounded-[20px]">
+		<div class="p-5 bg-primary-gradient rounded-[20px] hidden sm:flex" :class="expanded && '!flex'">
 			<img src="/public/qr-code.png" alt="qr code" class="rounded-[18px]" />
 		</div>
 	</div>
